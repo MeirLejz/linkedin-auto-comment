7df67dc38375ffd7be8d30c3eb5d4dbf57f6a88d
meir <meirlejzerowicz@gmail.com>
2025-05-26 06:24:14 +0300

    added import of Manage sub in index

--- website/src/index.js
+++ website/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Success from './pages/Success';
+import ManageSubscription from './pages/ManageSubscription';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
