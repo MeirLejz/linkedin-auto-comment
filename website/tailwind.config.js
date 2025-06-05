765d2b96b8599fcadc7d6966efc2753104fac687
meir <meirlejzerowicz@gmail.com>
2025-06-05 07:29:00 +0300

    trying to fix website ffs

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,13 +1,10 @@
-
-import type { Config } from "tailwindcss";
-
-export default {
+module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -104,4 +101,4 @@ export default {
     }
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
