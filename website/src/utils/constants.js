1bd2ab65e4dd53601eeec326214a6312a83bec30
meir <meirlejzerowicz@gmail.com>
2025-06-06 06:34:05 +0300

    started connection of website links

--- website/src/utils/constants.js
+++ website/src/utils/constants.js
@@ -1,10 +1,12 @@
 // URLs
 export const CHROME_STORE_URL = 'https://chromewebstore.google.com/detail/placeholder';
-export const PAYPRO_BASE_URL = 'https://store.payproglobal.com/checkout';
 export const REDIRECT_URL = 'https://linkedin-auto-comment.vercel.app/';
-export const LINKEDIN_URL = 'https://www.linkedin.com';
-export const PRIVACY_POLICY_URL = 'https://www.example.com/privacy';
-export const WHATSAPP_NUMBER = '+1234567890';
+export const LINKEDIN_URL = 'https://www.linkedin.com/company/avacom-ai/';
+export const PRIVACY_POLICY_URL = 'https://meirlejz.github.io/linkedin-comment-extension-privacy-policy.html';
+export const WHATSAPP_NUMBER = '+972522796834';
+
+export const PAYPRO_BASE_URL = 'https://store.payproglobal.com/checkout';
+
 
 // Product IDs
 export const PAYPRO_PRODUCT_ID = '112482';
